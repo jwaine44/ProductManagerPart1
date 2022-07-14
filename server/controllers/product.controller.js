b74418f5ff78e1bc9568bd3101ce15d7ed02817b
jwaine44 <104217670+jwaine44@users.noreply.github.com>
2022-07-14 15:55:35 -0400

    Initialize Repo

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,13 @@
+const { response } = require('express');
+const {Product} = require('../models/product.model');
+
+module.exports.createProduct = (req, res) => {
+    const {title, price, description} = req.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(product => res.json(product))
+        .catch(err => res.json(err))
+}
